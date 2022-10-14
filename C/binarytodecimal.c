23436f87cde24f21ddf7c0220be7c06bc4909cb8
rishabh-108272 <114498105+rishabh-108272@users.noreply.github.com>
2022-10-14 14:27:34 +0530

    Create binarytodecimal.c
    Program to convert binary number into decimal number.

---
+++ C/binarytodecimal.c
@@ -0,0 +1,24 @@
+/*Program to convert binary number to decimal.*/
+#include<stdio.h>
+
+int binarytodecimal(int n)
+{
+    int ans=0;
+    int x=1;
+    while(n>0)
+    {
+        int y=n%10;
+        ans+=x*y;
+        x*=2;
+        n/=10;
+    }
+    return ans;
+    }
+int main(){
+    int n;
+    printf("Enter the binary number:");
+    scanf("%d",&n);
+    int binarytodecimal(int n);
+    printf("Decimal number is:%d\n",binarytodecimal(n));
+    return 0;
+}
